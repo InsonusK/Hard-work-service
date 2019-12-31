d80d58689ec0246b2783240a38c24570a68b759e
Insonus.K <insonus.K@gmail.com>
2019-12-31 17:11:28 +0300

    add .travis push

---
+++ Scripts/Docker-push.sh
@@ -0,0 +1,6 @@
+﻿#!/usr/bin/env bash
+cd ../HardWorkService
+docker build -t insonusk/hard-work-service -f Dockerfile .
+
+echo "$DOCKER_PASSWORD" | docker login -u insonusk --password-stdin
+docker push insonusk/hard-work-service
