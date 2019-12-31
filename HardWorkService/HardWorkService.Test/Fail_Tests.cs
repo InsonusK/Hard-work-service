ff3316a2435febbb3d2eca7e116b40880bc675b5
Insonus.K <insonus.K@gmail.com>
2019-12-31 19:49:48 +0300

    add fail test to check travis

---
+++ HardWorkService/HardWorkService.Test/Fail_Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace HardWorkService.Test
+{
+    [TestFixture]
+    public class Fail_Tests
+    {
+        [Test]
+        public void Fail()
+        {
+            // Array
+            Assert.AreEqual(1,2);
+            // Act
+
+            // Assert
+
+        }
+    }
+}
