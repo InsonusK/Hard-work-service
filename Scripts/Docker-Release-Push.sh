53c39d0bdc43a554de9ea32ba3e8ac37af3cf7af
Insonus.K <insonus.K@gmail.com>
2019-12-31 18:29:54 +0300

    .travis another type of push

---
+++ Scripts/Docker-Release-Push.sh
@@ -0,0 +1,5 @@
+﻿#!/usr/bin/env bash
+ls
+docker build -t insonusk/hard-work-service -f ./HardWorkService/Dockerfile ./HardWorkService
+echo "$DOCKERHUBPASSWORD" | docker login -u insonusk --password-stdin
+docker push insonusk/hard-work-service
