7306d7f0fd14184aef7b4dc7c91224f5fa21f54e
Insonus.K <insonus.K@gmail.com>
2019-12-31 17:12:11 +0300

    Docker-push bugfix

--- Scripts/Docker-push.sh
+++ Scripts/Docker-push.sh
@@ -2,5 +2,5 @@
 cd ../HardWorkService
 docker build -t insonusk/hard-work-service -f Dockerfile .
 
-echo "$DOCKER_PASSWORD" | docker login -u insonusk --password-stdin
+echo "$DOCKERHUBPASSWORD" | docker login -u insonusk --password-stdin
 docker push insonusk/hard-work-service
