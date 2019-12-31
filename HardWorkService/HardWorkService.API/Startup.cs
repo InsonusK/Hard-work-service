6c196fc286d2a80dfc6d97b591f19fe1fcc8d162
Insonus.K <insonus.K@gmail.com>
2019-12-31 03:57:03 +0300

    Text changings

--- HardWorkService/HardWorkService.API/Startup.cs
+++ HardWorkService/HardWorkService.API/Startup.cs
@@ -53,7 +53,7 @@ namespace HardWorkService.API
 
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
             // specifying the Swagger JSON endpoint.
-            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"); });
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Hard work service API"); });
 
             app.UseRouting();
 
